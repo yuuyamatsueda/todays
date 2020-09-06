c21133a7aa11ef1653744193da85ad0c09430ffe
yuuya.matsueda <matsuedayuya0407@gmail.com>
2020-09-06 13:04:00 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+ @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Todays</h1>
@@ -8,4 +11,5 @@
             {!! link_to_route('signup.get', '新規登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
+    @endif
 @endsection
